500f1c0b942dabb4241ffcad16e4e969c4c6b3c7
Yuyu Zhou <yuyuzhouchina@gmail.com>
2015-12-14 19:20:58 +0000

    baremetal copying

---
+++ baremetal/init_baremetal_slave.sh
@@ -0,0 +1,9 @@
+current_ip=10.20.108.49
+current_pwd=/home/cc/new/scaletest/baremetal
+
+
+mkdir -p /home/cc/exp/baremetal/mantevo/script
+mkdir -p /home/cc/exp/baremetal/log
+mkdir -p /home/cc/exp/baremetal/mpich
+sudo yum install mpich -y
+cd /home/cc/exp/baremetal/mantevo/

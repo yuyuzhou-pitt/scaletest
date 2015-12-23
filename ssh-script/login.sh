2fb213431cfe8c544c0cce53699544b8fff470c0
Yuyu Zhou <yuyuzhou@Yuyus-MacBook-Air.local>
2015-12-23 11:34:52 -0500

    ssh script change for next round

--- ssh-script/login.sh
+++ ssh-script/login.sh
@@ -4,18 +4,18 @@
 #for ip in `cat ./hostfile`; do
 #    echo $ip
 #    ssh -t cc@$ip ssh-keygen
-#    sleep 5  
+#    sleep 10  
 #done
 
 #for ip in `cat ./hostfile`; do
 #    echo $ip
 #    ssh -t cc@$ip cat /home/cc/.ssh/id_rsa.pub >> ./log
-#    sleep 20
+#    sleep 10
 #done
 
 
-for host in `cat ./host_internal`; do
+for host in `cat ./hostfile`; do
     echo host=$host
-    ssh -t cc@$host scp 10.20.109.13:/home/cc/test ./
+    scp ./authorized_keys cc@$host:/home/cc/.ssh/authorized_keys
     sleep 10
 done
